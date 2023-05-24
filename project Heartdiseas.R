aaa65573bfa358507621fec57b415cc2e9b730d1
Nada Adel <nadaadel24657@gmail.com>
2023-05-24 09:30:55 +0300

    finished

--- project Heartdiseas.R
+++ project Heartdiseas.R
@@ -1,6 +1,10 @@
-# install.packages("factoextra")
+#############################################################
+#Install packages and loading libraries
+########################################
+
+#install.packages("factoextra")
 #install.packages("pheatmap")
-install.packages("dendextend")
+#install.packages("dendextend")
 
 library("factoextra")
 library(cluster)
@@ -9,26 +13,34 @@ library(ggplot2)
 library(pheatmap)
 library(dendextend)
 
-##########
-#Functions
+#############################################################
+#Declaring Functions
+######################
+
 min_max_scale <- function(x) {
   x=(x - min(x)) / (max(x) - min(x))
  
 }
 
-##################################
-###read file 
+#############################################################
+#read file 
+#############
 
 data<- read.table(header = TRUE,"Heartdiseas.txt",sep=",")
 
-########################
-#unnecessary columns
+#############################################################
+#############         preprocessing       ###################
+#############################################################
+
+#removing unnecessary columns
+###############################
 
 data$id<-NULL
 
-########################
+#############################################################
+#print columns include null values 
+####################################
 
-#know columns include null values 
 for (name in names(data))
 {
   if(sum (is.na(data[name]))>0)
@@ -37,22 +49,25 @@ for (name in names(data))
   }
 }
 
-############################
+#############################################################
 # result  columns include null  int 1 [1] "slope"
-#we can replace missing values by more frequency 
+#we can replace missing values by most frequent value 
+######################################################
+
 most_freq <- which.max(table(data["slope"]))
 data["slope"] <- replace(data["slope"], is.na(data["slope"]),most_freq )
 
 
-############
-############
+#############################################################
 ##Split Ages into Age Groups 
+###############################
+
 # data$age <- cut.default(data$age, breaks = c(0, 15, 30,45,60,75,90), labels = c(0, 1,2,3,4,5))
 # 
 # data$age<-as.numeric(as.character(data$age))
 
-##############
-#scale colums
+#############################################################
+#scale columns
 ################
 
 scaleName<-c("trestbps","chol","thalach","oldpeak")
@@ -61,29 +76,42 @@ for (i in scaleName){
   data[i]=min_max_scale(data[i])
 }
 
-###################################
+#############################################################
 #list
 typeof(data)
 class(data)
 cor(data)
 
-####################################
+#############################################################
+############# Visual representation of feature relationships
+#############################################################
+
 # Create a heatmap of the variable correlations
+###############################################
+
 heatmap(cor(data), scale = "none")
 
-######################################
-###ALL corr between thalach and columns in data  negative value 
+#############################################################
+#ALL corr between thalach and columns in data  negative value 
+#############################################################
 
 data$thalach<-NULL
 
-#####################################
+#############################################################
 #summary
+############
+
 summary(data)
 
-####################################
-#get The Optimal Number Of Clusters
+#############################################################
+#Evaluate the best number of clustering based on data set
+#############################################################
+
+#Find the Appropriate Number of Clusters
+#########################################
 
 wss <- numeric(15) 
+
 for (i in 1:15) 
   wss[i] <- sum(kmeans(data, centers=i)$withinss)
 
@@ -91,63 +119,54 @@ plot(1:15, wss, type="b", xlab="Number of Clusters",
      ylab="Within groups sum of squares")
 
 
-##################################
-#best number of cluster is 4 from plot 
+#############################################################
+# Execute the Kmeans Model
+#############################################################
 
 km<-kmeans(data, 4)
 
-####visualization of Kmeans clustering
+km
+km$center
 
 fviz_cluster(km, data, stand = FALSE, geom = "point")
 
-####################################
-####Hierarchical Clustering
+#############################################################
+#Hierarchical Clustering
+##########################
+
 dist_mat <- dist(data, method = 'euclidean')
-hclust_avg <- hclust(dist_mat, method = 'average')
-hclust_avg =as.dendrogram(hclust_avg)
-plot(hclust_avg)
-dend <- hclust_avg
-dend <- color_branches(dend, k = 7)
-dend <- color_labels(dend, k = 7)
-plot(dend)#4CLUSTERS
 
+fviz_dend(hclust(dist_mat), k = 4, show_labels = FALSE,
+          main="Hierarchical Clustering")
 
-###################################
-####Agglomerative Clustering using average
-avg = agnes(x=data, diss = FALSE, stand = TRUE,method = "average")
+#############################################################
+#Agglomerative Clustering using average
+#########################################
+
+avg = agnes(x=data, diss = FALSE, stand = TRUE, method = "average")
 avgDendrogram =as.dendrogram(avg)
-plot (avgDendrogram)
 
+fviz_dend(avgDendrogram, k = 7, show_labels = FALSE,
+          main="Agglomerative Clustering using average")
 
-dend <- avgDendrogram
-dend <- color_branches(dend, k = 5)
-dend <- color_labels(dend, k = 8)
-plot(dend) # 3CLUSTERS
+#############################################################
+#Agglomerative Clustering using single
+###########################################
 
-########################
-####Agglomerative Clustering using average
-single = agnes(x=data, diss = FALSE, stand = TRUE,method = "single")
+single = agnes(x=data, diss = FALSE, stand = TRUE, method = "single")
 singleDendrogram =as.dendrogram(single)
 
-plot(singleDendrogram)
+fviz_dend(singleDendrogram, k = 7, show_labels = FALSE,
+          main="Agglomerative Clustering using single")
 
+#############################################################
+#Agglomerative Clustering using complete 
+##########################################
 
-dend <- singleDendrogram
-dend <- color_branches(dend, k = 8)
-dend <- color_labels(dend, k = 8)
-plot(dend)# 2CLUSTERS
-
-################### 
-####Agglomerative Clustering using complete complete
-complete = agnes(x=data, diss = FALSE, stand = TRUE,method = "complete")
+complete = agnes(x=data, diss = FALSE, stand = TRUE, method = "complete")
 completeDendrogram =as.dendrogram(complete)
 
-plot(completeDendrogram)
+fviz_dend(completeDendrogram, k = 5, show_labels = FALSE,
+          main="Agglomerative Clustering using complete")
 
-dend <- completeDendrogram
-dend <- color_branches(dend, k = 8)
-dend <- color_labels(dend, k = 8)
-plot(dend) #5CLUSTERS
-
-###########################
-
+#############################################################
